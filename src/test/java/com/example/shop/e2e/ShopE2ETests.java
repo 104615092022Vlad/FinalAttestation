9eb15d4d69ce33f9922100049fa47c2ffda2b0cc
Vlad <ystimenko_vlad@mail.ru>
2023-07-19 15:38:21 +0300

    Moving UP! Check refresh button.

--- src/test/java/com/example/shop/e2e/ShopE2ETests.java
+++ src/test/java/com/example/shop/e2e/ShopE2ETests.java
@@ -4,20 +4,15 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.assertj.core.internal.Conditions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.Date;
 import java.util.Objects;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ShopE2ETests extends MainPage {
@@ -28,61 +23,71 @@ public class ShopE2ETests extends MainPage {
         Configuration.baseUrl = "http://localhost:63342/shop/shop.main/com/example/shop/ui/main.html?_ijt=d2uo4iijkc544camhctfilgkt0&_ij_reload=RELOAD_ON_SAVE";
     }
 
+    @BeforeEach
+    public void openBrowser() {
+        open("");
+    }
+
     @Test
     @DisplayName("Второй тест")
-    public void openShopsPage() throws InterruptedException{
-        open("");
+    public void openShopsPage() throws InterruptedException {
         $(refreshButton).click();
         Thread.sleep(5000);
     }
 
     @Test
-    @DisplayName("Кнопка обновления")
-    @RepeatedTest(10)
-    public void refreshButton() throws InterruptedException{
-        /*
-        1. Посчитать размер таблицы.
-        2. Добавить магазин.
-        3. Нажать кнопку "обновить".
-        4. Посчитать размер таблицы. Проверить, что он увеличился на 1.
-        5. Удалить магазин.
-        6. Посчитать размер таблицы. Проверить, что он уменьшился на 1.
-         */
-
-        open("");
-        //Thread.sleep(3000);
+    @DisplayName("Обновление списка текущих магазинов")
+    public void refreshButton() {
         allShopsTable.get(0).shouldBe(Condition.visible);
         int shopsQuantityBeforeAdding = allShopsTable.size();
-        $(createShopFiled).sendKeys("TestShop777");
-        $(createShopButton).click();
-        Selenide.switchTo().alert().accept();
-        $(refreshButton);
-        //Thread.sleep(3000);
+        Date currentTime = new Date();
+        String testShopName = "TestShop" + currentTime.getTime();
+
+        step("Создать тестовый магазин", () -> {
+            $(createShopFiled).sendKeys(testShopName);
+            $(createShopButton).click();
+            Selenide.switchTo().alert().accept();
+        });
+
+        step("Нажать на кнопку обновления", () -> {
+            assertThat($(refreshButton).exists()).isTrue();
+            $(refreshButton).click();
+        });
+
         allShopsTable.get(0).shouldBe(Condition.visible);
         int shopsQuantityAfterAdding = allShopsTable.size();
-        assertThat(shopsQuantityAfterAdding).isEqualTo(shopsQuantityBeforeAdding + 1);
+
+        step("Проверить изменения в таблице", () -> {
+            assertThat(shopsQuantityAfterAdding).isEqualTo(shopsQuantityBeforeAdding + 1);
+        });
 
         SelenideElement row;
         int i = 1;
         do {
             row = allShopsTable.get(shopsQuantityAfterAdding - i);
             i++;
-        } while(!Objects.equals(row.$("td:nth-child(2)").getAttribute("innerText"), "TestShop777"));
-
+        } while (!Objects.equals(row.$("td:nth-child(2)").getAttribute("innerText"), testShopName));
         String createdShopId = row.$("td:nth-child(1)").getAttribute("innerText");
 
-        $(deleteShopFiled).sendKeys(createdShopId);
-        $(deleteShopButton).click();
-        Selenide.switchTo().alert().accept();
+        step("Удалить тестовый магазин", () -> {
+            $(deleteShopFiled).sendKeys(createdShopId);
+            $(deleteShopButton).click();
+            Selenide.switchTo().alert().accept();
+        });
 
-        $(refreshButton);
-        allShopsTable.get(0).shouldBe(Condition.visible);
-        assertThat(allShopsTable.size()).isEqualTo(shopsQuantityBeforeAdding);
+        step("Нажать на кнопку обновления", () -> {
+            $(refreshButton).click();
+        });
+
+        step("Проверить изменения в таблице", () -> {
+            allShopsTable.get(0).shouldBe(Condition.visible);
+            assertThat(allShopsTable.size()).isEqualTo(shopsQuantityBeforeAdding);
+        });
     }
 
     @Test
     @DisplayName("Второй тест")
-    public void telegramButtonClick() throws InterruptedException{
+    public void telegramButtonClick() throws InterruptedException {
         open("");
         $(vkButton).click();
         Thread.sleep(5000);
